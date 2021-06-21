a9c94523edfdbc9b83e443b3aabc84c534fa025f
venusdharan <venusdharan@gmail.com>
2021-06-21 18:13:39 +0530

    changed loading

--- pages/class.vue
+++ pages/class.vue
@@ -30,7 +30,7 @@
 <section class="text-gray-600 body-font" style="min-height:90vh;" v-else>
   <div class="container px-5 py-24 mx-auto">
     <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-      <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Class Not Found</h1>
+      <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Loading Class Details</h1>
       <a class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0" href="/">Home</a>
     </div>
   </div>
