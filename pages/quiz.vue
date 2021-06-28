99058f845d331618a2be2efd93e43da5f1c34875
venusdharan <venusdharan@gmail.com>
2021-06-28 18:01:05 +0530

    added quiz

---
+++ pages/quiz.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="typeform-widget" data-url="https://form.typeform.com/to/N0KKEB7u?typeform-medium=embed-snippet" style="width: 100%; height: 90vh;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) }
+  },
+}
+</script>
